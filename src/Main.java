134379fdfd7beced8995cfbd5f9f016fa7b0dbaa
desalvtj <desalvtj@mail.uc.edu>
2024-09-27 18:41:12 -0400

    Final and complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        final int OLD_ENOUGH_FOR_BAND = 21;
+        int age = 0;
+        String trash = "";
+
+        System.out.print("Enter your age: ");
+
+        if(in.hasNextInt())
+        {
+           age = in.nextInt();
+           in.nextLine(); //clear the buffer
+            if(age >= OLD_ENOUGH_FOR_BAND)
+            {
+                System.out.println("You are 21 or older, you get a wrist band!");
+            }
+
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("You entered " + trash + ", which is not a valid entry. Run the program again with correct input.");
+        }
     }
 }
